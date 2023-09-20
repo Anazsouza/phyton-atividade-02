6f14498ad37280b4c3a3072288913e2238aded3b
Anazsouza <144919185+Anazsouza@users.noreply.github.com>
2023-09-20 12:17:16 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,15 @@
+print ("ola mundo")
+num1 = float(input("Digite o primeiro numero:"))
+num2 = float(input("Digite o segundo numero:"))
+
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+
+print(f"soma:{soma}")
+print(f"subtracao:{subtracao}")
+print(f"multiplicacao:{multiplicacao}")
+print(f"divisao: {divisao}")
